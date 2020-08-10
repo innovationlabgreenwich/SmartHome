8cda4312e3d2f0c2ef271e7059cec4a690324783
Greenwich Library Innovation Lab <68378504+innovationlabgreenwich@users.noreply.github.com>
2020-08-10 10:05:16 -0400

    Rename smarthome-lesson15.ino to smarthome-lesson-13.ino

---
+++ smarthome-lesson-13.ino
@@ -0,0 +1,143 @@
+/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
+ * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
+ *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
+ * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
+ *                  (____/ 
+ * Use browser and OSOYOO MEGA-IoT extension shield to control Relay from remote browser
+ * Tutorial URL http://osoyoo.com/?p=28969
+ * CopyRight www.osoyoo.com
+ */
+
+#include "WiFiEsp.h"
+//#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial softserial(A9, A8); // A9 to ESP_TX, A8 to ESP_RX by default
+//#endif
+#define RELAY_PIN 6
+
+char ssid[] = "******"; // replace ****** with your network SSID (name)
+char pass[] = "******"; // replace ****** with your network password
+int status = WL_IDLE_STATUS;
+
+int ledStatus = LOW;
+
+WiFiEspServer server(80);
+
+// use a ring buffer to increase speed and reduce memory allocation
+RingBuffer buf(8);
+
+void setup()
+{
+  pinMode(RELAY_PIN, OUTPUT);	// initialize digital pin RELAY_PIN as an output.
+  Serial.begin(9600);   // initialize serial for debugging
+    softserial.begin(115200);
+  softserial.write("AT+CIOBAUD=9600\r\n");
+  softserial.write("AT+RST\r\n");
+  softserial.begin(9600);    // initialize serial for ESP module
+  WiFi.init(&softserial);    // initialize ESP module
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+  
+  // start the web server on port 80
+  server.begin();
+}
+
+
+void loop()
+{
+  WiFiEspClient client = server.available();  // listen for incoming clients
+
+  if (client) {                               // if you get a client,
+    Serial.println("New client");             // print a message out the serial port
+    buf.init();                               // initialize the circular buffer
+    while (client.connected()) {              // loop while the client's connected
+      if (client.available()) {               // if there's bytes to read from the client,
+        char c = client.read();               // read a byte, then
+        buf.push(c);                          // push it to the ring buffer
+    //  Serial.print(c);
+        // printing the stream to the serial monitor will slow down
+        // the receiving of data from the ESP filling the serial buffer
+        //Serial.write(c);
+        
+        // you got two newline characters in a row
+        // that's the end of the HTTP request, so send a response
+        if (buf.endsWith("\r\n\r\n")) {
+          sendHttpResponse(client);
+          break;
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (buf.endsWith("GET /H")) {
+          Serial.println("Turn RELAY ON");
+          ledStatus = HIGH;
+          digitalWrite(RELAY_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+        }
+        else if (buf.endsWith("GET /L")) {
+          Serial.println("Turn RELAY OFF");
+          ledStatus = LOW;
+          digitalWrite(RELAY_PIN, LOW);    // turn the LED off by making the voltage LOW
+        }
+      }
+    }
+    
+    // close the connection
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+
+void sendHttpResponse(WiFiEspClient client)
+{
+  // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+  // and a content-type so the client knows what's coming, then a blank line:
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-type:text/html");
+  client.println();
+  
+  // the content of the HTTP response follows the header:
+  client.print("RELAY ");
+  if (ledStatus==HIGH) 
+    client.print("ON");
+  else client.print("OFF");
+  client.println("<p>");
+  client.println("Click <a href=\"/H\">here</a> turn the RELAY on<br>");
+  client.println("Click <a href=\"/L\">here</a> turn the RELAY off<br>");
+  
+  // The HTTP response ends with another blank line:
+  client.println();
+}
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in the browser
+  Serial.println();
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+  Serial.println();
+}
